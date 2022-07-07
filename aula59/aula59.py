13224a12125603ff7a8b36e87db14b037b22d5b8
NDMO1 <2004ndof@gmail.com>
2022-07-07 12:41:35 +0100

    Aula

--- aula59/aula59.py
+++ aula59/aula59.py
@@ -2,3 +2,21 @@
 Nesta aula o tema abordado é:
 Funções decoradoras e decoradores
 """
+
+def master(funcao):
+    def slave(*args, **kwargs):
+        print('SIUUU')
+        funcao(*args, **kwargs)
+
+    return slave
+
+@master
+def oi():
+    print('Hello World')
+
+@master
+def outra_func(msg):
+    print(msg)
+
+outra_func('Olá eu sou o Daniel')
+
