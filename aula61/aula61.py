abaa56e8b718435028fec0bf4909f4061c6bfe7d
NDMO1 <2004ndof@gmail.com>
2022-07-07 15:06:47 +0100

    Aula

--- aula61/aula61.py
+++ aula61/aula61.py
@@ -2,3 +2,7 @@
 Nesta aula o tema abordado é:
 Classes - Python Orientado a Objetos
 """
+from pessoa import Pessoa
+
+p1 = Pessoa('Daniel', 18)
+
---
+++ aula61/pessoa.py
@@ -0,0 +1,12 @@
+class Pessoa:
+    def __int__(self, nome, idade, comer=False, falar=False):
+        self.nome = nome
+        self.idade = idade
+        self.comer = comer
+        self.falar = falar
+
+        variavel = 'Valor'
+        print(variavel)
+
+    def outro_metodo(self):
+        print
