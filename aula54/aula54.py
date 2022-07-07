c5c87f726f76c1e78b53de7f9a894b23766bd9c7
NDMO1 <2004ndof@gmail.com>
2022-07-07 10:01:05 +0100

    Aula

--- aula54/aula54.py
+++ aula54/aula54.py
@@ -5,6 +5,17 @@ Módulos são arquivos .py (que contém código python) e servem para expandir
 as funcionalidades padrão da linguaguem.
 Todos os módulos padrão:
 https://docs.python.org/3/py-modindex.html
+Não é recomendado utilizar o * ao importar módulos
+Exemplo:
+from random import *
 """
 
-print()
+import random
+
+
+def randint(*args):
+    return 'hahaha'
+
+
+for i in range(10):
+    print(random.randint(0, 10))
---
+++ aula55/aula55.py
@@ -0,0 +1,4 @@
+"""
+Nesta aula o tema abordado é:
+Como criar módulos em Python
+"""
