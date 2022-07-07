73a46db4d34e67a09ab06fc1fdc0a0e5e7f3e436
NDMO1 <2004ndof@gmail.com>
2022-07-07 10:24:04 +0100

    Aula

---
+++ aula55/app.py
@@ -0,0 +1,7 @@
+from aula55 import multiplica, dobro_lista, PI
+from outro import oi
+
+print(PI)
+print(dobro_lista([2, 4]))
+print(multiplica([2, 4]))
+oi()
--- aula55/aula55.py
+++ aula55/aula55.py
@@ -2,3 +2,24 @@
 Nesta aula o tema abordado é:
 Como criar módulos em Python
 """
+import math
+
+PI = math.pi
+
+
+def dobro_lista(lista):
+    return [x * 2 for x in lista]
+
+
+def multiplica(lista):
+    r = 1
+    for i in lista:
+        r *= 1
+    return r
+
+
+if __name__ == '__main__':
+    lista = [1, 2, 3, 4, 5]
+    print(dobro_lista(lista))
+    print(multiplica(lista))
+    print(PI)
